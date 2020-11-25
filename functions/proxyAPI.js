af7c40431a7ed0118e34fc9766f3921ca3db5fa3
thobyv-kismat <vivee18@gmail.com>
2020-11-25 20:21:33 +0100

    first deploy

---
+++ functions/proxyAPI.js
@@ -0,0 +1,72 @@
+const fetch = require('node-fetch');
+
+const GITHUB_API_KEY = '44d524fea61c67f6e58ccc0effbf60bddc47d633';
+const GITHUB_GRAPH_API_URL = 'https://api.github.com/graphql';
+const GRAPH_QUERY = {
+    "query": `{
+viewer {
+    name
+    login
+    bio
+    avatarUrl
+    location
+    url
+    company
+    repositories(orderBy: {field: PUSHED_AT, direction: DESC}, first: 10, isFork: false) {
+    totalCount
+    nodes {
+        id
+        name
+        description
+        url
+        stargazers {
+        totalCount
+        }
+        forks {
+        totalCount
+        }
+        languages(first: 5) {
+        nodes {
+            name
+        }
+        }
+        pushedAt 
+    }
+    }
+}
+}`
+};
+
+exports.handler = async function (event, context) {
+    try {
+        const response = await fetch(GITHUB_GRAPH_API_URL, {
+            method: 'POST',       
+            body: JSON.stringify(GRAPH_QUERY),     
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'authorization': 'token ' + GITHUB_API_KEY
+            }
+        })
+
+        if (!response.ok) {
+            return {
+                statusCode: response.status,
+                body: response.statusText,
+            }
+        }
+        const data = await response.json();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data),
+        }
+
+    } catch (err) {
+        console.log(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify(err),
+        }
+    }
+};
